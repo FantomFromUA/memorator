ba688e48e2afda47fad3416a80314d940ad83246
Eric Toroni <erictoroni@Erics-MacBook-Pro.local>
2025-12-31 12:28:28 +0100

    feat: hello

---
+++ src/main/java/com/memorator/MemoratorApplication.java
@@ -0,0 +1,13 @@
+package com.memorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memorator/MemoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
